b77fe5f2c57d655a200064dcd253a4b354829c5b
airbus <ichbrauchemehrplatz@gmail.com>
2022-08-16 23:17:19 +0200

    started react on client and ajuster prettier config on server and client

---
+++ client/src/game.ts
@@ -0,0 +1,10 @@
+interface GameProps {
+    game: {
+        name: string;
+        minPlayers: number;
+    };
+}
+
+export function Game({ game }: GameProps) {
+    return null;
+}
---
+++ client/src/games-library.ts
@@ -0,0 +1,9 @@
+const games = [
+    { name: 'Catan', minPlayers: 2 },
+    { name: 'Catan 2', minPlayers: 3 },
+    { name: 'Dixit', minPlayers: 4 },
+];
+
+export function GamesLibrary() {
+    return null;
+}
