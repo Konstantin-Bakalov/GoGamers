582448f3590c33bcc11131f08594fbcb756d8f70
airbus <ichbrauchemehrplatz@gmail.com>
2022-09-01 22:47:16 +0200

    added zod string as number and prettier ignore to server, client and shared

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,1 +1,2 @@
-export * from "./models/game-input";
+export * from './models/game-input';
+export * from './lib/zod-validator';
--- shared/src/models/game-input.ts
+++ shared/src/models/game-input.ts
@@ -1,9 +1,9 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const CreateGameInputSchema = z.object({
-  name: z.string().min(3),
-  minAge: z.number().positive(),
-  genres: z.array(z.string()).min(1),
+    name: z.string().min(3),
+    minAge: z.number().positive(),
+    genres: z.array(z.string()).min(1),
 });
 
 export type CreateGameInput = z.infer<typeof CreateGameInputSchema>;
