ee388c7da908e441bef00386a305a266688f161d
airbus <ichbrauchemehrplatz@gmail.com>
2022-10-18 22:11:03 +0200

    created migration for media

--- server/migrations/20221018191308_add_image_to_game.ts
+++ server/migrations/20221018191308_add_image_to_game.ts
@@ -1,9 +1,27 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    // await knex.schema.alterTable(table => {
-    //     table.text();
-    // })
+    await knex.schema.createTable('media', (table) => {
+        table.increments('id').primary();
+
+        table
+            .integer('game_id')
+            .references('id')
+            .inTable('games')
+            .notNullable()
+            .onDelete('CASCADE');
+
+        table
+            .enu('type', ['image', 'video'], {
+                useNative: true,
+                enumName: 'media_type',
+            })
+            .alter();
+
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    });
 }
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists('media');
+}
