567b3c6600297de2447ee95b9950a32f5f9f1d76
airbus <ichbrauchemehrplatz@gmail.com>
2022-10-19 12:10:08 +0200

    implemented inserting image when a game is created

--- shared/src/models/game-input.ts
+++ shared/src/models/game-input.ts
@@ -4,6 +4,8 @@ export const CreateGameInputSchema = z.object({
     name: z.string().min(3),
     minAge: z.number().positive(),
     genres: z.array(z.string()).min(1),
+    url: z.string(),
+    type: z.literal('image').or(z.literal('video')),
 });
 
 export type CreateGameInput = z.infer<typeof CreateGameInputSchema>;
