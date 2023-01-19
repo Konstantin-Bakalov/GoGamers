7ee8fdd8ea7ccbbcb7ce22ef0199d2a19c818414
airbus <ichbrauchemehrplatz@gmail.com>
2023-01-19 19:30:02 +0100

    created footer component

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,20 +1,33 @@
 import { Box, Paper, Typography } from '@mui/material';
 import { makeStyles } from '../lib/make-styles';
+import Link from '@mui/material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const githubLink = 'https://github.com/Konstantin-Bakalov';
 
 const styles = makeStyles({
     box: {
         martinTop: 'auto',
     },
     paper: {
-        // borderRadius: '0px',
+        marginTop: '3rem',
+        minHeight: '45px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '1rem',
     },
 });
 
 export function Footer() {
     return (
         <Box sx={styles.box}>
-            <Paper>
-                <Typography sx={styles.paper}>Footer Text</Typography>
+            <Paper sx={styles.paper}>
+                <Typography>Copyright © 2023 Konstantin Bakalov</Typography>
+
+                <Link target="_blank" rel="noopener" href={githubLink}>
+                    <GitHubIcon />
+                </Link>
             </Paper>
         </Box>
     );
