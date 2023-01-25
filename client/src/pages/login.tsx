848857e4f69d6a327e37d179688bfde01f6711c8
airbus <ichbrauchemehrplatz@gmail.com>
2023-01-25 23:01:24 +0100

    created image for the login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,8 +1,19 @@
-import { Alert, Box } from '@mui/material';
+import { Alert, Box, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { authService } from '../services/auth-service';
 import { GoogleLogin } from '@react-oauth/google';
 import { useState } from 'react';
+import logo from '../images/controller.png';
+import { makeStyles } from '../lib/make-styles';
+
+const styles = makeStyles({
+    picture: {
+        objectFit: 'cover',
+        objectPosition: 'center',
+        height: '800px',
+        width: '800px',
+    },
+});
 
 export function Login() {
     const [invalidCredential, setInvalidCredential] = useState(false);
@@ -10,10 +21,32 @@ export function Login() {
     const navigate = useNavigate();
 
     return (
-        // TODO: Remove margin
-        <Box sx={{ marginTop: '64px', height: '100%', backgroundColor: 'red' }}>
+        <Box sx={{ marginTop: '64px' }}>
             {invalidCredential && <Alert>Invalid Credentials</Alert>}
             {loginFailed && <Alert>Login Failed</Alert>}
+
+            <Box sx={{ display: 'flex' }}>
+                <Box>
+                    <Typography sx={{ fontWeight: 700 }} variant="h2">
+                        Your personal
+                    </Typography>
+                    <Typography
+                        sx={{ fontWeight: 700 }}
+                        color="background.paper"
+                        variant="h2"
+                    >
+                        gaming library
+                    </Typography>
+                </Box>
+
+                <Box>
+                    <Typography>
+                        Discover and store games of all sorts
+                    </Typography>
+                </Box>
+
+                <Box sx={styles.picture} component="img" src={logo} />
+            </Box>
 
             <GoogleLogin
                 onSuccess={async (credentialResponse) => {
