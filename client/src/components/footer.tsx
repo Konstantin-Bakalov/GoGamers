dba49e7c46dd5d0b6921de744b46588a46c4d436
airbus <ichbrauchemehrplatz@gmail.com>
2023-01-25 19:10:05 +0100

    finished footer component

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -6,19 +6,23 @@ const styles = makeStyles({
         martinTop: 'auto',
     },
     paper: {
+        backgroundColor: 'primary.main',
         marginTop: '3rem',
         minHeight: '45px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
     },
+    text: {
+        color: 'background.paper',
+    },
 });
 
 export function Footer() {
     return (
         <Box sx={styles.box}>
-            <Paper square sx={styles.paper}>
-                <Typography variant="body1">
+            <Paper elevation={24} square sx={styles.paper}>
+                <Typography sx={styles.text}>
                     Copyright © 2023 Konstantin Bakalov
                 </Typography>
             </Paper>
