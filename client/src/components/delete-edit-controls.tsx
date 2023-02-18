30baeb73682384f78958626b65f3f9a1f1323123
airbus <ichbrauchemehrplatz@gmail.com>
2023-02-18 16:46:13 +0100

    added tooltip to edit and delete buttons

--- client/src/components/delete-edit-controls.tsx
+++ client/src/components/delete-edit-controls.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Tooltip } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -13,13 +13,21 @@ export function DeleteEditControls({
 }: DeleteEditControlsProps) {
     return (
         <Box>
-            <IconButton size="large" aria-label="edit game" onClick={onEdit}>
-                <EditIcon color="primary" />
-            </IconButton>
+            <Tooltip placement="top" title="Edit">
+                <IconButton
+                    size="large"
+                    aria-label="edit game"
+                    onClick={onEdit}
+                >
+                    <EditIcon color="primary" />
+                </IconButton>
+            </Tooltip>
 
-            <IconButton size="large" aria-label="delete" onClick={onDelete}>
-                <DeleteIcon color="primary" />
-            </IconButton>
+            <Tooltip placement="top" title="Delete">
+                <IconButton size="large" aria-label="delete" onClick={onDelete}>
+                    <DeleteIcon color="primary" />
+                </IconButton>
+            </Tooltip>
         </Box>
     );
 }
