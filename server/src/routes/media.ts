cd5cf63e32bdf94dea2ebae22e5c655ca622bb12
Konstantin <constantin.bakalov@gmail.com>
2024-04-27 15:03:13 +0200

    configured coudinary to use https instead of http for media

--- server/src/routes/media.ts
+++ server/src/routes/media.ts
@@ -8,23 +8,24 @@ export const mediaRouter = Router();
 type MediaType = 'image' | 'video';
 
 cloudinary.config({
-    cloud_name: config.get('cloudinary.name'),
-    api_key: config.get('cloudinary.key'),
-    api_secret: config.get('cloudinary.secret'),
+  cloud_name: config.get('cloudinary.name'),
+  api_key: config.get('cloudinary.key'),
+  api_secret: config.get('cloudinary.secret'),
 });
 
 mediaRouter.post(
-    '/',
-    requestHandler(async (req, res) => {
-        const media = req.body.media;
-        const type: MediaType = req.body.type;
+  '/',
+  requestHandler(async (req, res) => {
+    const media = req.body.media;
+    const type: MediaType = req.body.type;
 
-        const result = await cloudinary.uploader.upload(media, {
-            upload_preset: config.get('cloudinary.preset'),
-            discard_original_filename: true,
-            resource_type: type,
-        });
+    const result = await cloudinary.uploader.upload(media, {
+      upload_preset: config.get('cloudinary.preset'),
+      discard_original_filename: true,
+      resource_type: type,
+      secure: true,
+    });
 
-        res.status(200).json({ url: result.url });
-    }),
+    res.status(200).json({ url: result.url });
+  })
 );
